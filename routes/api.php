955b8665dc196b60f20c2994d66d474362ac2bb4
Luttje <2738114+luttje@users.noreply.github.com>
2023-01-26 15:50:02 +0100

    cache api results

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// e.g: http://localhost:8000/api/v1/weather/51.571915/4.768323
+// e.g: http://localhost:8000/api/v1/weather/51.571915/4.768323/1600000000
 Route::get('/weather/{latitude}/{longitude}/{whenUnixTimestamp?}', function($latitude, $longitude, $whenUnixTimestamp = null) {
     $weather = \App\Models\Weather::getAtCoordinates($latitude, $longitude, $whenUnixTimestamp);
 
