2337766579124992f17a29d47732e72bdede8f0b
Luttje <2738114+luttje@users.noreply.github.com>
2023-01-26 12:15:32 +0100

    activities+migration+seeder+spatie-tags

---
+++ app/Models/Activity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
+
+class Activity extends Model
+{
+    use HasFactory, 
+        HasTags;
+}
