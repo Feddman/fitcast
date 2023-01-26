0cba74073c48d157d7c1d88647f019fc884b8130
Luttje <2738114+luttje@users.noreply.github.com>
2023-01-26 14:38:14 +0100

    default time now

--- resources/views/components/input/date-time.blade.php
+++ resources/views/components/input/date-time.blade.php
@@ -6,5 +6,9 @@
       class="start-time border border-gray-400 p-2 w-full"
       type="datetime-local"
       name="starttime"
-    >
+      value="{{ now()->format('Y-m-d\TH:i') }}"
+      step="60"
+      min="{{ now()->format('Y-m-d\TH:i')  }}"
+      max="{{ now()->addDay()->format('Y-m-d\TH:i') }}"
+    />
 </div>
